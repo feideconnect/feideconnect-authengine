e169e3fced2c2f4ef0b696e639ff85d8d9acf943
Andreas Åkre Solberg <andreas.solberg@uninett.no>
2015-01-18 22:25:43 +0100

    Attempt to setup php-fpm and phantomjs for CI testing.

---
+++ tests-phantomjs/main.js
@@ -0,0 +1,21 @@
+
+// for now this just tests to conncet on a localhost ....
+
+
+var config = {
+	"url": "https://127.0.0.1/"
+};
+var page = require('webpage').create();
+
+page.onResourceRequested = function(request) {
+  console.log('Request ' + JSON.stringify(request, undefined, 4));
+};
+page.onResourceReceived = function(response) {
+  console.log('Receive ' + JSON.stringify(response, undefined, 4));
+};
+
+page.open(config.url + 'oauth/authorization', function() {
+	
+	phantom.exit();
+});
+
