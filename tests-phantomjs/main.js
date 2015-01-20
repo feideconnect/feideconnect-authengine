ba910bc5f23adbff216f27107dd274bf7eccd6c3
Andreas Åkre Solberg <andreas.solberg@uninett.no>
2015-01-20 20:48:52 +0100

    Supress debug output

--- tests-phantomjs/main.js
+++ tests-phantomjs/main.js
@@ -2,10 +2,6 @@ var system = require('system');
 var page = require('webpage').create();
 var LoginFlow = require('./lib/LoginFlow').LoginFlow;
 
-
-console.log("sys"+ system.env["password"]);
-
-
 page.viewportSize = { width: 1080, height: 720 };
 
 
