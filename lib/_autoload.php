c1ca4e6fb48965c8fc8ae779e3f192fadc7fedbf
Andreas Åkre Solberg <andreas.solberg@uninett.no>
2015-06-22 13:41:29 +0200

    add errorhandler that logs errors through connect log routines, and add set_magic_quotes_runtime()

--- lib/_autoload.php
+++ lib/_autoload.php
@@ -41,6 +41,15 @@ spl_autoload_register(function ($class) {
     }
 });
 
+
+
+if (get_magic_quotes_runtime()) {
+    set_magic_quotes_runtime(FALSE);
+}
+
+
+
+
 require_once(__DIR__ . '/profiler.php');
 require_once(dirname(__DIR__) . '/vendor/autoload.php');
 require_once(dirname(dirname(__DIR__)) . '/simplesamlphp/lib/_autoload.php');
@@ -48,3 +57,67 @@ require_once(dirname(dirname(__DIR__)) . '/simplesamlphp/lib/_autoload.php');
 // error_reporting(E_ERROR | E_WARNING);
 
 
+
+use FeideConnect\Logger;
+
+/* Log full backtrace on errors and warnings. */
+function Connect_error_handler($errno, $errstr, $errfile = NULL, $errline = 0, $errcontext = NULL) {
+
+    static $limit = 5;
+    $limit -= 1;
+    if ($limit < 0) {
+        /* We have reached the limit in the number of backtraces we will log. */
+        return FALSE;
+    }
+
+    var_dump($errcontext); exit;
+
+
+    switch ($errno) {
+        case E_USER_ERROR:
+
+            Logger::error($errstr, [
+                "type" => "phperror",
+                "line" => $errline,
+                "file" => $errfile,
+                "context" => $errcontext
+            ]);
+
+            exit(1);
+            break;
+
+        case E_USER_WARNING:
+
+            Logger::warning($errstr, [
+                "type" => "phperror",
+                "line" => $errline,
+                "file" => $errfile,
+                "context" => $errcontext
+            ]);
+            break;
+
+        case E_USER_NOTICE:
+
+            Logger::info($errstr, [
+                "type" => "phperror",
+                "line" => $errline,
+                "file" => $errfile,
+                "context" => $errcontext
+            ]);
+            break;
+
+        default:
+
+            Logger::info($errstr, [
+                "errno" => $errno,
+                "type" => "phperror",
+                "line" => $errline,
+                "file" => $errfile,
+                "context" => $errcontext
+            ]);
+            break;
+    }
+
+    return false;
+}
+set_error_handler('Connect_error_handler');
