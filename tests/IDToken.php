a6dcdd5a54e0bc66502550e1cea1fceab88ea409
Andreas Åkre Solberg <andreas.solberg@uninett.no>
2015-08-25 12:18:55 +0200

    Add basic unit test to verify IDtoken class.

---
+++ tests/IDToken.php
@@ -0,0 +1,53 @@
+<?php
+
+
+namespace tests;
+
+use FeideConnect\OpenIDConnect\IDToken;
+use FeideConnect\OpenIDConnect\TrustStore;
+use FeideConnect\Config;
+
+// use FeideConnect\Router;
+// use FeideConnect\HTTP\JSONResponse;
+// use FeideConnect\Data\StorageProvider;
+// use FeideConnect\Data\Models;
+
+putenv("AEENV=test");
+if (getenv('AEENV') !== "test") { 
+	throw new \Exception("Not able to set environmentvariable for test environment."); 
+}
+
+class IDTokenTest extends \PHPUnit_Framework_TestCase {
+
+	protected $truststore;
+
+
+	function __construct() {
+
+
+		$this->truststore = new TrustStore();
+
+	}
+
+    public function testIDToken() {
+
+
+    	$iss = Config::getValue('connect.issuer');
+    	// TrustStore $trustStore, $iss, $sub, $aud, $expiresIn, $auth_time = null) {
+    	$idtoken = IDToken::generate($this->truststore, $iss, 'http://sp.example.org', 3600, null);
+    	$idtxt = $idtoken->getEncoded();
+
+    	// echo "IDToken is " . $idtxt . "\n";
+
+    	$this->assertInternalType('string', $idtxt, 'ID TOken string is text');
+    	$this->assertGreaterThan(80, strlen($idtxt), 'ID Token string is more than 80 character long');
+
+    	// var_dump($idtoken);
+
+
+    }
+
+
+
+
+}
