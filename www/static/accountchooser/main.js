f34ea9b0cc2711dd8a7f3b8d3615640ceeefdd4b
Andreas Åkre Solberg <andreas.solberg@uninett.no>
2015-08-25 22:02:08 +0200

    Fix IE<=9, by defining console when dev console is not open

--- www/static/accountchooser/main.js
+++ www/static/accountchooser/main.js
@@ -3,6 +3,15 @@ define(function(require, exports, module) {
 
 	require('components/es6-promise/promise.min');
 
+
+	// Configure console if not defined. A fix for IE <= 9.
+	if (!window.console) {
+		window.console = {
+			"log": function() {},
+			"error": function() {},
+		}
+	}
+
     var App = require('App');
 	$(document).ready(function() {
 	    var app = new App();
--- www/static/oauthgrant/main.js
+++ www/static/oauthgrant/main.js
@@ -1,6 +1,14 @@
 define(function(require, exports, module) {
 	"use strict";
 
+	// Configure console if not defined. A fix for IE <= 9.
+	if (!window.console) {
+		window.console = {
+			"log": function() {},
+			"error": function() {},
+		}
+	}
+	
 	var App = require('App');
 	$(document).ready(function() {
 	    var app = new App();
