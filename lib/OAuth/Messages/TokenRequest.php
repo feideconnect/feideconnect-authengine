52726586d0715fd3c04deba822b4be83bdebe3e8
Sigmund Augdal <sigmund.augdal@uninett.no>
2015-10-13 14:12:36 +0200

    Fix strangely unalinged assignments

--- lib/OAuth/Messages/TokenRequest.php
+++ lib/OAuth/Messages/TokenRequest.php
@@ -11,17 +11,17 @@ class TokenRequest extends Message {
 
         parent::__construct($message);
 
-        $this->grant_type        = Message::prequire($message, 'grant_type', array('authorization_code', 'refresh_token', 'client_credentials', 'password'));
-        $this->code             = Message::optional($message, 'code');
-        $this->redirect_uri        = Message::optional($message, 'redirect_uri');
-        $this->client_id        = Message::optional($message, 'client_id');
-        $this->client_secret    = Message::optional($message, 'client_secret');
+        $this->grant_type = Message::prequire($message, 'grant_type', array('authorization_code', 'refresh_token', 'client_credentials', 'password'));
+        $this->code = Message::optional($message, 'code');
+        $this->redirect_uri = Message::optional($message, 'redirect_uri');
+        $this->client_id = Message::optional($message, 'client_id');
+        $this->client_secret = Message::optional($message, 'client_secret');
 
-        $this->username    = Message::optional($message, 'username');
-        $this->password    = Message::optional($message, 'password');
+        $this->username = Message::optional($message, 'username');
+        $this->password = Message::optional($message, 'password');
 
         // Used in Client Credentials Grant flow.
-        $this->scope        = Message::spacelist(Message::optional($message, 'scope'));
+        $this->scope = Message::spacelist(Message::optional($message, 'scope'));
 
     }
 
