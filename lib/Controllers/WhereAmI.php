2ccd25384665fb63708f0c57b29bf1f516e19553
Sigmund Augdal <sigmund.augdal@uninett.no>
2015-10-26 16:23:00 +0100

    Clean up WhereAmI controller which contains some copy/pasted code that wasn't used

--- lib/Controllers/WhereAmI.php
+++ lib/Controllers/WhereAmI.php
@@ -10,30 +10,10 @@ use FeideConnect\GeoLocation;
 
 class WhereAmI {
 
-
     public static function process() {
-
-        $data = array();
-        // $data["disco"] = Config::readJSONfile("disco.json");
-        // $data["return"] = $_REQUEST["return"];
-        // $data["returnIDParam"] = $_REQUEST["returnIDParam"];
-
         $l = new GeoLocation();
 
         return new JSONResponse($l->getLocation());
-
-
-
-
-        // if (isset($_REQUEST["isPassive"]) && $_REQUEST["isPassive"] === "true") {
-        //     // The correct behaviour of the IdP Discovery Protocol will be to return
-        //     // without the "returnIDParam" parameter set to anything.
-        //     // If the disco stores preferences, we might return that instead.
-        //     return new Redirect($data["return"]);
-        // }
-
-        // return (new TemplatedHTMLResponse('disco'))->setData($data);
-
     }
 
 }
