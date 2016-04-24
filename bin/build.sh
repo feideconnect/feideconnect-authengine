8355888d84cb1b96232fb2095fe562c7aaf0be6e
Andreas Åkre Solberg <andreas.solberg@uninett.no>
2016-04-24 20:15:07 +0200

    Use updated image name

--- bin/build.sh
+++ bin/build.sh
@@ -4,10 +4,10 @@ echo "Stopping existing image"
 docker stop dae && docker rm dae
 
 echo "Building new image"
-docker build -t andreassolberg/dataporten-authengine .
+docker build -t eu.gcr.io/turnkey-cocoa-720/dataporten-authengine .
 
 echo "Running container"
-docker run -p 80:80 -p 443:443 -d --name dae --env-file=./ENV -t andreassolberg/dataporten-authengine
+docker run -p 80:80 -p 443:443 -d --name dae --env-file=./ENV -t eu.gcr.io/turnkey-cocoa-720/dataporten-authengine
 
 echo "Opening shell"
 docker exec -i -t dae bash
