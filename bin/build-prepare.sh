e34e82a184ee7a59cb138af5cab015731d2454f3
Andreas Åkre Solberg <andreas.solberg@uninett.no>
2016-04-24 20:14:18 +0200

    Add script to prepare dependencies before building

---
+++ bin/build-prepare.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+
+echo "Stopping existing image"
+git clone git@scm.uninett.no:feide-connect/dataporten-resources.git
+
+git clone git@scm.uninett.no:feide-connect/dataporten-config.git
