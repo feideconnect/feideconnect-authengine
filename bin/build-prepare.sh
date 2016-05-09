92f292704d879292ef23043834056cdace58485a
Andreas Åkre Solberg <andreas.solberg@uninett.no>
2016-05-09 11:29:07 +0200

    update build prepare

--- bin/build-prepare.sh
+++ bin/build-prepare.sh
@@ -2,5 +2,14 @@
 
 echo "Load resources"
 git clone git@scm.uninett.no:feide-connect/dataporten-resources.git
+
 echo "Load config"
 git clone git@scm.uninett.no:feide-connect/dataporten-config.git
+
+
+mkdir var
+cp -r dataporten-config/authengine-dev/simplesamlphp-certs var/
+cp -r dataporten-config/authengine-dev/web-certs var/
+
+cp -r dataporten-config/authengine-dev/simplesamlphp-config etc/
+cp -r dataporten-config/authengine-dev/simplesamlphp-metadata etc/
