047b51b966700b6ddf063e9f487c332c07327182
Olav Morken <olav.morken@uninett.no>
2016-05-12 16:13:06 +0200

    Add infrastructure for running the authengine tests.

---
+++ run-tests.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+set -e
+cd "$(dirname "${BASH_SOURCE[0]}")"
+
+docker-compose up -d
+function clean-docker() {
+    docker-compose kill
+    docker-compose rm --force --all
+}
+trap clean-docker EXIT
+
+CASSANDRA_PORT=$(docker-compose port cassandra 9042 | sed 's@.*:@@')
+CASSANDRA="localhost:${CASSANDRA_PORT}"
+
+echo "Cassandra available on ${CASSANDRA}"
+echo "Waiting schema setup to complete"
+while ! docker-compose ps 2>/dev/null | grep -q 'dataportenschemas.*Exit'; do
+    sleep 0.1
+done
+echo "Done"
+
+if [ ! -d simplesamlphp ]; then
+    curl https://simplesamlphp.org/res/downloads/simplesamlphp-1.14.0.tar.gz | tar xz
+    mv simplesamlphp-1.14.0 simplesamlphp
+fi
+
+mkdir -p etc/test
+sed "s/@@CASSANDRA@@/${CASSANDRA}/" <test-config/auth-engine-config.json >etc/test/config.json
+cp test-config/jwt-*.pem etc
+
+ant
