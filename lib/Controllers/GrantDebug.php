1257df3575d090170d84c062d17d3992e6d4cebf
Andreas Åkre Solberg <andreas.solberg@uninett.no>
2016-06-08 10:30:39 +0200

    Minor update to grant debug info

--- lib/Controllers/GrantDebug.php
+++ lib/Controllers/GrantDebug.php
@@ -23,18 +23,52 @@ class GrantDebug {
 
     public static function debug() {
 
-
-
         // $test = new ScopesInspector(['gk_foodle']);
         // echo '<pre>';
         // print_r($test->getInfo());
         // exit;
+        // http://localhost/
+
+        $userid = '07a97f30-4f90-4870-9190-a7ec0bc88dba'; // Andreas
+        $attributes = [
+            "eduPersonPrincipalName" => ["andreas@uninett.no"],
+            "jpegPhoto" => null,
+            "mail" => ["andreas.solberg@uninett.no"],
+            "displayName" => ["Andreas Åkre Solberg"],
+            "o" => ["UNINETT"],
+            "feideYearOfBirth" => ["1980"],
+            "idp" => "https://idp-test.feide.no",
+            "authSource" => "default-sp"
+        ];
+
+
+
+        // $userid = '9d9e77d0-01ca-4b2f-aac0-2a3442d40aaa';
+        // $attributes = [
+        //     "twitter.id_str" => ["6405972"],
+        //     "twitter.name" => ["Andreas Åkre Solberg"],
+        //     "twitter.screen_name" => ["erlang"],
+        //     "twitter.location" => ["Trondheim, Norway"],
+        //     "twitter.description" => ["Identity, Authentication, Authorization, API, OAuth, OpenID Connect and more..."],
+        //     "twitter.url" => ["https://t.co/kGigyNc7DR"],
+        //     "twitter.created_at" => ["Tue May 29 07:25:20 +0000 2007"],
+        //     "twitter_at_screen_name" => ["@erlang"],
+        //     "twitter.profile_image_url" => ["http://pbs.twimg.com/profile_images/36002982/1387422937_3dccca0331_m_normal.jpg"],
+        //     "authSource" => "twitter"];
 
 
 
 
-        $userid = '76a7a061-3c55-430d-8ee0-6f82ec42501f'; // Andreas
-        $clientid = '610cbba7-3985-45ae-bc9f-0db0e36f71ad'; // Foodle
+
+
+
+
+
+        $clientid = '74b8b784-5350-41de-adf8-2b83dcecd34e'; // Foodle
+
+
+
+
 
         $storage = StorageProvider::getStorage();
         $user = $storage->getUserByUserID($userid);
@@ -45,21 +79,17 @@ class GrantDebug {
             'client_id' => $clientid
         ]);
 
-        $attributes = [
-            "eduPersonPrincipalName" => ["andreas@uninett.no"],
-            "jpegPhoto" => null,
-            "mail" => ["andreas.solberg@uninett.no"],
-            "displayName" => ["Andreas Åkre Solberg"],
-            "o" => ["UNINETT AS"],
-            "feideYearOfBirth" => ["1980"],
-            "idp" => "https://idp-test.feide.no",
-            "authSource" => "default-sp",
-            "o" => ["UNINETT AS"]
-        ];
+
+
+        // echo '<pre>Data'; print_r($attributes); exit;
+
         $attributeMapper = new AttributeMapper();
         $account = $attributeMapper->getAccount($attributes);
+        // echo '<pre>'; print_r($account); exit;
 
         $org = $account->getOrg();
+
+        // echo '<pre>ORG:'; var_dump($org); exit;
 
         $ae = new AuthorizationEvaluator($storage, $client, $request, $user);
         $aui = new AuthorizationUI($client, $request, $account, $user, $ae);
@@ -68,7 +98,7 @@ class GrantDebug {
             ->setFixedBypass(false)
             ->setFixedFirstTime(true)
             ->setFixedSimpleView(false)
-            ->setFixedMandatory(true)
+            ->setFixedMandatory(false)
             ->show();
 
         return $response;
