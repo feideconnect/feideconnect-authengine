a9b97014c1a0214bb256cc5c322d88d62994399b
Olav Morken <olav.morken@uninett.no>
2016-09-19 14:42:21 +0200

    RawCassandra2: Add rawExecute()-function, for running INSERT-commands.

--- tests/RawCassandra2.php
+++ tests/RawCassandra2.php
@@ -3,6 +3,16 @@
 namespace tests;
 
 class RawCassandra2 extends \FeideConnect\Data\Repositories\Cassandra2 {
+    public function rawExecute($query, $params) {
+        $this->db->querySync(
+            $query,
+            $params,
+            \Cassandra\Request\Request::CONSISTENCY_QUORUM,
+            [
+                'names_for_values' => true
+            ]
+        );
+    }
     public function rawQuery($query, $params) {
         $response = $this->db->querySync(
             $query,
