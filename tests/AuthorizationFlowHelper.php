bc66f387db7fb15a60c4c35cb5501f9bce510311
Sigmund Augdal <sigmund.augdal@uninett.no>
2016-09-21 13:36:50 +0200

    Refactor out some reusable code out of OAuthAuthorizationCodeTest.php

---
+++ tests/AuthorizationFlowHelper.php
@@ -0,0 +1,128 @@
+<?php
+namespace tests;
+
+use FeideConnect\Router;
+use FeideConnect\HTTP\JSONResponse;
+use FeideConnect\Authentication\AuthSource;
+
+class AuthorizationFlowHelper extends DBHelper {
+
+    protected $client;
+
+    public function setUp() {
+        parent::setUp();
+        $_SERVER['REMOTE_ADDR'] = '127.0.0.1';
+        $_SERVER['REQUEST_URI'] = '/foo';
+        $_SERVER['REQUEST_METHOD'] = 'GET';
+        $_SERVER['SERVER_PROTOCOL'] = 'HTTP/1.1';
+        $_SERVER['HTTP_ACCEPT_LANGUAGE'] = 'en-US';
+
+        $_REQUEST['state'] = '06dad165-7d22-4dcf-bda9-38f4048b9e3d';
+        $_POST['redirect_uri'] = 'http://example.org';
+        $_REQUEST['redirect_uri'] = 'http://example.org';
+
+        $this->client = $this->client();
+        $_REQUEST['client_id'] = $this->client->id;
+
+        $this->user = $this->user();
+        AuthSource::setFactory(['\tests\MockAuthSource', 'create']);
+    }
+
+    protected function doAuthorizationRequest($overrides = null) {
+        $_REQUEST['acresponse'] = '{"id": "https://idp.feide.no","subid":"example.org", "userids": ["feide:testuser@example.org"]}';
+        $_REQUEST['verifier'] = $this->user->getVerifier();
+        $_REQUEST['bruksvilkar'] = 'yes';
+        $_REQUEST['approved_scopes'] = 'userinfo groups';
+
+        if (!empty($overrides)) {
+            foreach ($overrides as $k => $v) {
+                $_REQUEST[$k] = $v;
+            }
+        }
+        $router = new Router();
+
+        return $router->dispatchCustom('GET', '/oauth/authorization');
+    }
+
+    public function testAuthorizationToAccountChooser() {
+        $this->setExpectedExceptionRegExp(
+            'FeideConnect\Exceptions\RedirectException',
+            '/^http:\/\/localhost\/accountchooser\?/'
+        );
+        $response = $this->doAuthorizationRequest(['acresponse' => null]);
+        var_export($response);
+    }
+
+    public function testAuthorizationToConsent() {
+        $router = new Router();
+
+
+        $response = $this->doAuthorizationRequest(['approved_scopes' => null, 'verifier' => null, 'bruksvilkår' => null]);
+        $this->assertInstanceOf('FeideConnect\HTTP\LocalizedTemplatedHTMLResponse', $response, 'Expected /oauth/authorization endpoint to return html');
+
+        $data = $response->getData();
+        $this->assertArrayHasKey('posturl', $data);
+        $this->assertEquals('http://localhost/oauth/authorization', $data['posturl']);
+        $this->assertArrayHasKey('needsAuthorization', $data);
+        $this->assertTrue($data['needsAuthorization']);
+    }
+
+    protected function getAuthorizationCode($overrides=null) {
+        $response = $this->doAuthorizationRequest($overrides);
+        $this->assertInstanceOf('FeideConnect\HTTP\Redirect', $response, 'Expected /oauth/authorization endpoint to redirect');
+
+//        var_export($response);
+        $url = $response->getURL();
+        $this->assertEquals("http", parse_url($url, PHP_URL_SCHEME));
+        $this->assertEquals("example.org", parse_url($url, PHP_URL_HOST));
+        $query = parse_url($url, PHP_URL_QUERY);
+        parse_str($query, $params);
+        $this->assertArrayHasKey('state', $params);
+        $this->assertEquals($_REQUEST['state'], $params['state']);
+        $this->assertArrayHasKey('code', $params);
+        return $params['code'];
+
+    }
+
+    public function testNoEffectiveScopes() {
+
+        $overrides = [
+            'approved_scopes' => 'email',
+            'scope' => 'email',
+        ];
+
+        $response = $this->doAuthorizationRequest($overrides);
+
+        $this->assertInstanceOf('FeideConnect\HTTP\JSONResponse', $response, 'Expected /oauth/token endpoint to return json');
+
+        $this->assertEquals(400, $response->getStatus());
+        $data = $response->getData();
+        $this->assertEquals('invalid_scope', $data['error']);
+    }
+
+    protected function assertTokenEndpointResponseOK($response) {
+        $this->assertInstanceOf('FeideConnect\HTTP\JSONResponse', $response, 'Expected /oauth/token endpoint to return json');
+
+        $data = $response->getData();
+        $this->assertEquals(200, $response->getStatus());
+
+        $this->assertArrayHasKey('access_token', $data);
+        $this->assertArrayHasKey('token_type', $data);
+        $this->assertArrayHasKey('expires_in', $data);
+        $this->assertArrayHasKey('scope', $data);
+        $this->assertEquals('Bearer', $data['token_type']);
+    }
+
+    protected function assertTokenAccessDenied($response) {
+        $this->assertInstanceOf('FeideConnect\HTTP\JSONResponse', $response, 'Expected /oauth/token endpoint to return json');
+
+        $data = $response->getData();
+        $this->assertEquals(401, $response->getStatus());
+
+    }
+
+    public function tearDown() {
+        parent::tearDown();
+        $this->db->removeClient($this->client);
+    }
+}
--- tests/OAuthAuthorizationCodeTest.php
+++ tests/OAuthAuthorizationCodeTest.php
@@ -5,122 +5,30 @@ use FeideConnect\Router;
 use FeideConnect\HTTP\JSONResponse;
 use FeideConnect\Authentication\AuthSource;
 
-class OAuthAuthorizationCodeTest extends DBHelper {
+class OAuthAuthorizationCodeTest extends AuthorizationFlowHelper {
 
     protected $client;
 
     public function setUp() {
         parent::setUp();
-        $_SERVER['REMOTE_ADDR'] = '127.0.0.1';
-        $_SERVER['REQUEST_URI'] = '/foo';
-        $_SERVER['REQUEST_METHOD'] = 'GET';
-        $_SERVER['SERVER_PROTOCOL'] = 'HTTP/1.1';
-        $_SERVER['HTTP_ACCEPT_LANGUAGE'] = 'en-US';
-
         $_REQUEST['response_type'] = 'code';
-        $_REQUEST['state'] = '06dad165-7d22-4dcf-bda9-38f4048b9e3d';
-        $_POST['redirect_uri'] = 'http://example.org';
-        $_REQUEST['redirect_uri'] = 'http://example.org';
-
-        $this->client = $this->client();
-        $_REQUEST['client_id'] = $this->client->id;
-
-        $this->user = $this->user();
-        AuthSource::setFactory(['\tests\MockAuthSource', 'create']);
-    }
-
-    public function testAuthorizationToAccountChooser() {
-        $this->setExpectedExceptionRegExp(
-            'FeideConnect\Exceptions\RedirectException',
-            '/^http:\/\/localhost\/accountchooser\?/'
-        );
-        $router = new Router();
-
-        $response = $router->dispatchCustom('GET', '/oauth/authorization');
-    }
-
-    public function testAuthorizationToConsent() {
-        $router = new Router();
-
-        $_REQUEST['acresponse'] = '{"id": "https://idp.feide.no","subid":"example.org", "userids": ["feide:testuser@example.org"]}';
-
-        $response = $router->dispatchCustom('GET', '/oauth/authorization');
-        $this->assertInstanceOf('FeideConnect\HTTP\LocalizedTemplatedHTMLResponse', $response, 'Expected /oauth/authorization endpoint to return html');
-
-        $data = $response->getData();
-        $this->assertArrayHasKey('posturl', $data);
-        $this->assertEquals('http://localhost/oauth/authorization', $data['posturl']);
-        $this->assertArrayHasKey('needsAuthorization', $data);
-        $this->assertTrue($data['needsAuthorization']);
-    }
-
-    public function testAuthorizationToCode() {
-        $router = new Router();
-
-        $_REQUEST['acresponse'] = '{"id": "https://idp.feide.no","subid":"example.org", "userids": ["feide:testuser@example.org"]}';
-        $_REQUEST['verifier'] = $this->user->getVerifier();
-        $_REQUEST['bruksvilkar'] = 'yes';
-        $_REQUEST['approved_scopes'] = 'userinfo groups';
-
-        $response = $router->dispatchCustom('GET', '/oauth/authorization');
-        $this->assertInstanceOf('FeideConnect\HTTP\Redirect', $response, 'Expected /oauth/authorization endpoint to redirect');
-
-//        var_export($response);
-        $url = $response->getURL();
-        $this->assertEquals("http", parse_url($url, PHP_URL_SCHEME));
-        $this->assertEquals("example.org", parse_url($url, PHP_URL_HOST));
-        $query = parse_url($url, PHP_URL_QUERY);
-        parse_str($query, $params);
-        $this->assertArrayHasKey('state', $params);
-        $this->assertEquals($_REQUEST['state'], $params['state']);
-        $this->assertArrayHasKey('code', $params);
-        return $params['code'];
-
-    }
-
-    public function testNoEffectiveScopes() {
-        $router = new Router();
-
-        $_REQUEST['acresponse'] = '{"id": "https://idp.feide.no","subid":"example.org", "userids": ["feide:testuser@example.org"]}';
-        $_REQUEST['verifier'] = $this->user->getVerifier();
-        $_REQUEST['bruksvilkar'] = 'yes';
-        $_REQUEST['approved_scopes'] = 'email';
-        $_REQUEST['scope'] = 'email';
-
-        $response = $router->dispatchCustom('GET', '/oauth/authorization');
-
-        $this->assertInstanceOf('FeideConnect\HTTP\JSONResponse', $response, 'Expected /oauth/token endpoint to return json');
-
-        $this->assertEquals(400, $response->getStatus());
-        $data = $response->getData();
-        $this->assertEquals('invalid_scope', $data['error']);
     }
 
     public function testTokenBasicAuthOK() {
         $router = new Router();
-        $code = $this->testAuthorizationToCode();
+        $code = $this->getAuthorizationCode();
 
         $_SERVER['PHP_AUTH_USER'] = $this->client->id;
         $_SERVER['PHP_AUTH_PW'] = $this->client->client_secret;
         $_POST['grant_type'] = 'authorization_code';
         $_POST['code'] = $code;
         $response = $router->dispatchCustom('POST', '/oauth/token');
-
-        $this->assertInstanceOf('FeideConnect\HTTP\JSONResponse', $response, 'Expected /oauth/token endpoint to return json');
-
-        $data = $response->getData();
-        $this->assertEquals(200, $response->getStatus());
-
-        $this->assertArrayHasKey('access_token', $data);
-        $this->assertArrayHasKey('token_type', $data);
-        $this->assertArrayHasKey('expires_in', $data);
-        $this->assertArrayHasKey('scope', $data);
-        $this->assertEquals('Bearer', $data['token_type']);
+        $this->assertTokenEndpointResponseOK($response);
     }
 
     public function testTokenBasicAuthWrongClientId() {
         $router = new Router();
-        $code = $this->testAuthorizationToCode();
+        $code = $this->getAuthorizationCode();
 
         $_SERVER['PHP_AUTH_USER'] = "wrong";
         $_SERVER['PHP_AUTH_PW'] = $this->client->client_secret;
@@ -128,16 +36,12 @@ class OAuthAuthorizationCodeTest extends DBHelper {
         $_POST['code'] = $code;
         $response = $router->dispatchCustom('POST', '/oauth/token');
 
-        $this->assertInstanceOf('FeideConnect\HTTP\JSONResponse', $response, 'Expected /oauth/token endpoint to return json');
-
-        $data = $response->getData();
-        $this->assertEquals(401, $response->getStatus());
-
+        $this->assertTokenAccessDenied($response);
     }
 
     public function testTokenBasicAuthWrongClientSecret() {
         $router = new Router();
-        $code = $this->testAuthorizationToCode();
+        $code = $this->getAuthorizationCode();
 
         $_SERVER['PHP_AUTH_USER'] = $this->client->id;
         $_SERVER['PHP_AUTH_PW'] = "wrong";
@@ -145,16 +49,12 @@ class OAuthAuthorizationCodeTest extends DBHelper {
         $_POST['code'] = $code;
         $response = $router->dispatchCustom('POST', '/oauth/token');
 
-        $this->assertInstanceOf('FeideConnect\HTTP\JSONResponse', $response, 'Expected /oauth/token endpoint to return json');
-
-        $data = $response->getData();
-        $this->assertEquals(401, $response->getStatus());
-
+        $this->assertTokenAccessDenied($response);
     }
 
     public function testTokenPOSTOK() {
         $router = new Router();
-        $code = $this->testAuthorizationToCode();
+        $code = $this->getAuthorizationCode();
 
         $_POST['client_id'] = $this->client->id;
         $_POST['client_secret'] = $this->client->client_secret;
@@ -162,21 +62,12 @@ class OAuthAuthorizationCodeTest extends DBHelper {
         $_POST['code'] = $code;
         $response = $router->dispatchCustom('POST', '/oauth/token');
 
-        $this->assertInstanceOf('FeideConnect\HTTP\JSONResponse', $response, 'Expected /oauth/token endpoint to return json');
-
-        $data = $response->getData();
-        $this->assertEquals(200, $response->getStatus(), var_export($data, true));
-
-        $this->assertArrayHasKey('access_token', $data);
-        $this->assertArrayHasKey('token_type', $data);
-        $this->assertArrayHasKey('expires_in', $data);
-        $this->assertArrayHasKey('scope', $data);
-        $this->assertEquals('Bearer', $data['token_type']);
+        $this->assertTokenEndpointResponseOK($response);
     }
 
     public function testTokenPOSTWrongClientId() {
         $router = new Router();
-        $code = $this->testAuthorizationToCode();
+        $code = $this->getAuthorizationCode();
 
         $_POST['client_id'] = "wrong";
         $_POST['client_secret'] = $this->client->client_secret;
@@ -184,16 +75,12 @@ class OAuthAuthorizationCodeTest extends DBHelper {
         $_POST['code'] = $code;
         $response = $router->dispatchCustom('POST', '/oauth/token');
 
-        $this->assertInstanceOf('FeideConnect\HTTP\JSONResponse', $response, 'Expected /oauth/token endpoint to return json');
-
-        $data = $response->getData();
-        $this->assertEquals(401, $response->getStatus());
-
+        $this->assertTokenAccessDenied($response);
     }
 
     public function testTokenPOSTWrongClientSecret() {
         $router = new Router();
-        $code = $this->testAuthorizationToCode();
+        $code = $this->getAuthorizationCode();
 
         $_POST['client_id'] = $this->client->id;
         $_POST['client_secret'] = "wrong";
@@ -201,15 +88,7 @@ class OAuthAuthorizationCodeTest extends DBHelper {
         $_POST['code'] = $code;
         $response = $router->dispatchCustom('POST', '/oauth/token');
 
-        $this->assertInstanceOf('FeideConnect\HTTP\JSONResponse', $response, 'Expected /oauth/token endpoint to return json');
-
-        $data = $response->getData();
-        $this->assertEquals(401, $response->getStatus());
-
+        $this->assertTokenAccessDenied($response);
     }
 
-    public function tearDown() {
-        parent::tearDown();
-        $this->db->removeClient($this->client);
-    }
 }
