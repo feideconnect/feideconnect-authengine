7b468c1f5c8ac65d5261152524b3748065fe5e74
Sigmund Augdal <sigmund.augdal@uninett.no>
2016-11-02 16:55:45 +0100

    Remove commented out code containing references to uwap Some of these where considered by internet explorer as containing syntax errors

--- lib/HTTP/TemplatedHTMLResponse.php
+++ lib/HTTP/TemplatedHTMLResponse.php
@@ -19,7 +19,6 @@ class TemplatedHTMLResponse extends HTTPResponse {
         $partialsDir = Config::dir('templates/partials');
         $mustache = new \Mustache_Engine(array(
             'loader' => new \Mustache_Loader_FilesystemLoader($templateDir),
-            // 'cache' => '/tmp/uwap-mustache',
             'partials_loader' => new \Mustache_Loader_FilesystemLoader($partialsDir),
         ));
         $this->template = $mustache->loadTemplate($templateName);
--- www/static/oauthgrant/main.js
+++ www/static/oauthgrant/main.js
@@ -16,14 +16,3 @@ define(function(require, exports, module) {
 
 
 });
-
-
-// require( ['jquery', 'uwap-core/bootstrap3/js/bootstrap', 'uwap-core/bootstrap3/js/modal', 'uwap-core/bootstrap3/js/dropdown'], function( $ ) {
-
-// 	$(document).ready(function() {
-// 		$('#submit').on('click', function() {
-// 			$("form").submit();
-// 		});
-// 	});
-
-// });
