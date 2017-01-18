957b61bbf0e1c64158602d9a74a1ed1d80c42661
Andreas Åkre Solberg <andreas.solberg@uninett.no>
2017-01-18 16:29:15 +0100

    Update ConfigTest to match new config.

--- tests/ConfigTest.php
+++ tests/ConfigTest.php
@@ -32,9 +32,8 @@ class ConfigTest extends \PHPUnit_Framework_TestCase {
   //       echo "oooooo";
         // echo("........==> " . Config::getValue('test.foo.lo'));
 
-        $this->assertTrue(Config::getValue('test.bar', true) === true, 'Config picking not existing prop should return default');
-        $this->assertTrue(Config::getValue('test.foo.lo') === 3, 'Config read test.foo.lo === 3');
-
+        $this->assertTrue(Config::getValue('test', true) === true, 'Config picking not existing prop should return default');
+        $this->assertTrue(Config::getValue('storage.type') === 'cassandra', 'Config read storage.type === cassandra');
         $this->assertTrue(Config::getValue('test.foo.li', 3) === 3, 'Config read fall back to default param');
 
         // $this->assertTrue(Config::getValue('test.foo.li', null, true) === 3, 'should throw exceptoin');
