2cd7ced72db0a79e57240fca124c34368f541f5c
Sigmund Augdal <sigmund.augdal@uninett.no>
2017-10-11 15:15:42 +0200

    Update translation strategy
    * Use UNINETT organization for translation
    * Use tx cli tool rather than grunt to push/pull translations
    * Leave it to transifex to insert the original string where translations are missing

--- lib/Localization.php
+++ lib/Localization.php
@@ -20,7 +20,7 @@ class Localization {
         if (Config::getValue('enableLocalization', false)) {
             $availableLanguages = Config::getValue('availableLanguages', ['en']);
             $lang = Misc::getBrowserLanguage($availableLanguages);
-            $dictionaryFile = Config::filepath('dictionaries/build/dictionary.' . $lang . '.json');
+            $dictionaryFile = Config::filepath('dictionaries/dictionary.' . $lang . '.json');
         } else {
             $lang = 'en';
             $dictionaryFile = Config::filepath('dictionaries/dictionary.en.json');
