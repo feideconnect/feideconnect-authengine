f844aa1915450f588dd19f619acd10c1296c9004
Andreas Åkre Solberg <andreas.solberg@uninett.no>
2017-10-24 12:16:19 +0200

    Remove deprecated shell scripts for use with kubernetes

--- bin/_config.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/bash
-
-export KUBERNETES_DEPLOYMENT="dataporten-authengine"
-export VERSION="1.1.2"
-export IMAGE="eu.gcr.io/turnkey-cocoa-720/$KUBERNETES_DEPLOYMENT:$VERSION"
-# export IMAGE="dataporten-authengine"
--- bin/shell.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-
-export RUN_PATH=`dirname "$0" || echo .`
-set -a
-. ${RUN_PATH}/_config.sh
-set +a
-
-docker exec -ti ${KUBERNETES_DEPLOYMENT} bash
-# docker run -ti --link devenv_cassandra_1:cassandra ${IMAGE} bash
