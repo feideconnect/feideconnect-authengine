f62c6846d0a5d13c13407d8ce8d02cea1ae07f14
Andreas Åkre Solberg <andreas.solberg@uninett.no>
2017-11-01 17:54:25 +0100

    Include dustjs path, and stop using almond.

--- build.js
+++ build.js
@@ -1,12 +1,13 @@
 ({
-    "name": "../components/almond/almond",
+    "name": "../components/requirejs/require",
     "wrap": true,
     "insertRequire": ["main"],
     "include": "main",
     "paths": {
       "jquery": "../components/jquery/dist/jquery.min",
       "bootstrap": "../components/bootstrap/dist/js/bootstrap.min",
-      "tooltip": "../components/bootstrap/js/tooltip"
+      "tooltip": "../components/bootstrap/js/tooltip",
+      "dust": "../components/dustjs-linkedin/dist/dust-full.min"
     },
     "shim": {
         "bootstrap": ["jquery"],
