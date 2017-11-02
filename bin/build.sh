db0c45fe17de98560feddb26abb9b1cce1989a23
John-Magne Bredal <john.m.bredal@uninett.no>
2017-11-02 09:57:56 +0100

    Bundle css
    Files in css/src/ and css/lib bundled into two files for now. No minifying

--- bin/build.sh
+++ bin/build.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
-clear
-node_modules/requirejs/bin/r.js -o build.js baseUrl=./www/static/accountchooser out=./www/static/build/accountchooser.min.js
-node_modules/requirejs/bin/r.js -o build.js baseUrl=./www/static/oauthgrant out=./www/static/build/oauthgrant.min.js
-node_modules/requirejs/bin/r.js -o cssIn=./www/static/css/style.css out=./www/static/build/style.min.css
+set -x
+
+mkdir -p ./www/static/build
+
+# Minify javascript and css files
+# node_modules/requirejs/bin/r.js -o build.js baseUrl=./www/static/accountchooser out=./www/static/build/accountchooser.min.js
+# node_modules/requirejs/bin/r.js -o build.js baseUrl=./www/static/oauthgrant out=./www/static/build/oauthgrant.min.js
+# node_modules/requirejs/bin/r.js -o cssIn=./www/static/css/src/style.css out=./www/static/css/src/style.min.css
+
+# Bundle css-files
+find ./www/static/css/lib/ -name '*.css' -exec cat {} + > ./www/static/build/bundle-lib.css
+find ./www/static/css/src/ -name '*.css' -exec cat {} + > ./www/static/build/bundle-src.css
--- setup-container.sh
+++ setup-container.sh
@@ -1,5 +1,6 @@
 #! /bin/sh
 set -e
+set -x
 curl -sL https://deb.nodesource.com/setup_6.x | bash
 apt-get install -y nodejs
 npm install npm@latest -g
