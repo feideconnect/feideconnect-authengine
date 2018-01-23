d5fb6c9668aa55c9b279c7b26f1afdee9086d055
Andreas Åkre Solberg <andreas.solberg@uninett.no>
2018-01-23 18:12:22 +0100

    Clean up debug logging

--- bin/build.sh
+++ bin/build.sh
@@ -6,9 +6,6 @@ mkdir -p www/static/build
 
 # Dust templates
 #./node_modules/dustjs-linkedin/bin/dustc --amd templates/*.dust > www/static/accountchooser/templates.js
-ls -la www/static/components
-ls -la www/static/components
-ls -la www/static/components/dustjs-linkedin/bin/
 
 www/static/components/dustjs-linkedin/bin/dustc templates/dust_providerlist.dust www/static/build/dust_providerlist.dust.js
 www/static/components/dustjs-linkedin/bin/dustc templates/dust_accountlist.dust www/static/build/dust_accountlist.dust.js
