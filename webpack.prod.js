d02f4447f3694dac5251fa426ae20999b3e09a9e
John-Magne Bredal <john.m.bredal@uninett.no>
2018-01-29 12:56:13 +0100

    Introduce hash in webpack.prod output

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,13 +1,18 @@
 const webpack = require('webpack');
 const merge = require('webpack-merge');
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+const AssetsPlugin = require('assets-webpack-plugin')
 const common = require('./webpack.common');
 
 module.exports = merge(common, {
+    output: {
+        filename: '[name].[chunkhash].js'
+    },
     plugins: [
         new UglifyJSPlugin(),
         new webpack.DefinePlugin({
             'process.env.NODE_ENV': JSON.stringify('production')
-        })
+        }),
+        new AssetsPlugin()
     ]
 })
